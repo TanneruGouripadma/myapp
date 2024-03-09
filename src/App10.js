780cb9db475c1ef425e830cc624455f443494725
gouri <gouripadma@lyrostech.com>
2024-03-09 16:48:16 +0530

    .

--- src/App10.js
+++ src/App10.js
@@ -1,19 +1,18 @@
-import react from "react";
+import React from "react";
 import Feeds from "./Feeds";
 import Post from "./Post";
-import { BrowserRouter, Routes, Route, Link} from "react -router -dom";
-export default function App10()
-{
-    return (
-        <div>
-            <BrowserRouter>
-            <Link to="/">Feeds</Link> <Link to="/post">Post</Link>
-            <Routes>
-                <Route path ="/" index element={<Feeds/>}></Route>
-                <Route path="/post" element={<post/>}></Route>
-            </Routes>
-            </BrowserRouter>
-        </div>
-    );
-    
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+export default function App15() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Link to="/">Feeds</Link> | <Link to="/post">Post</Link>
+        <Routes>
+          
+            <Route path="/post" element={<Post/>}></Route>
+            <Route path="/" index element={<Feeds/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 // import App from './App1';
 // import App1 from './App1';
 // import App6 from './App6';
-// import App9 from '.App9';
-import App10 from '.App10';
+import App9 from './App9';
+import App10 from './App10';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App1 name="raja" age="23"/> */}
     {/* <App6/> */}
-    {/* <App9/> */}
     <App10/>
+    {/* <App10/> */}
   </React.StrictMode>
 );
 
